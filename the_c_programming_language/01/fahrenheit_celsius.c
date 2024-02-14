5ce008edb13969be3f2edd264f426955c11566d5
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-14 18:34:35 -0300

    convert from celsius from fahrenheit

--- the_c_programming_language/01/fahrenheit_celsius.c
+++ the_c_programming_language/01/fahrenheit_celsius.c
@@ -10,6 +10,8 @@ int main() {
     int upper = 300;
     int step = 20;
 
+    printf("from Fahrenheit to Celsius:\n\n");
+
     fahr = lower;
 
     while (fahr <= upper) {
@@ -17,4 +19,15 @@ int main() {
         printf("%3.0f %6.1f\n", fahr, celsius);
         fahr += step;
     }
+
+    printf("\nfrom Celsius to Fahrenheit:\n\n");
+
+    celsius = lower;
+
+    while (celsius <= upper) {
+        fahr = celsius * (9.0 / 5.0) + 32.0;
+        printf("%3.0f %6.1f\n", celsius, fahr);
+        celsius += step;
+    }
+    
 }
