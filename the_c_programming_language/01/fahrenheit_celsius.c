e04c5e0d50c782083eeaed6cbc21ee768405b08c
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-15 08:06:23 -0300

    add symbolic constants

--- the_c_programming_language/01/fahrenheit_celsius.c
+++ the_c_programming_language/01/fahrenheit_celsius.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 /* printf Fahrenheit-Celsius table
     for fahr = 0, 20, ..., 300*/
 
 int main() {
     float fahr, celsius;
 
-    int lower = 0;
-    int upper = 300;
-    int step = 20;
-
     printf("from Fahrenheit to Celsius:\n\n");
 
-    fahr = lower;
+    fahr = LOWER;
 
-    while (fahr <= upper) {
+    while (fahr <= UPPER) {
         celsius = 5.0 * (fahr - 32.0) / 9.0;
         printf("%3.0f %6.1f\n", fahr, celsius);
-        fahr += step;
+        fahr += STEP;
     }
 
     printf("\nfrom Celsius to Fahrenheit:\n\n");
 
-    celsius = lower;
+    celsius = LOWER;
 
-    while (celsius <= upper) {
+    while (celsius <= UPPER) {
         fahr = celsius / 5.0 * 9.0 + 32.0;
         printf("%3.0f %6.1f\n", celsius, fahr);
-        celsius += step;
+        celsius += STEP;
     }
     
     printf("\nfrom Fahrenheit to Celsius (reverse table):\n\n");
     
-    for (fahr = upper; fahr >= lower; fahr -= step) {
+    for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
         printf("%3.0f %6.1f\n", fahr, 5.0 * (fahr - 32) / 9);
     }
 
     printf("\nfrom Celsius to Fahrenheit (reverse table):\n\n");
     
-    for (celsius = upper; celsius >= lower; celsius -= step) {
+    for (celsius = UPPER; celsius >= LOWER; celsius -= STEP) {
         printf("%3.0f %6.1f\n", celsius, celsius / 5 * 9 + 32);
     }
 }
