aefe93f15df22a8cc92e6abc5992cf62ef008270
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-21 12:12:56 -0300

    add a reverse line function

--- the_c_programming_language/01/char_array.c
+++ the_c_programming_language/01/char_array.c
@@ -2,18 +2,30 @@
 
 #define MAXLINE 1000
 #define LONGEST_CRITERION 80
+#define REMOVE_NULL_CHAR 1
 
 int getln(char line[], int maxline);
 void copy(char to[], char from[]);
+
+void reverse(char s[], char to[], int size) {
+    int i;
+
+    for (i = 0; i < size; i++) {
+        to[i] = s[(size - i) - REMOVE_NULL_CHAR];
+    }
+}
 
 int main(void) {
     int len;
     int max;
     char line[MAXLINE];
     char longest[MAXLINE];
+    char reverse_line[MAXLINE];
 
     max = 0;
     while ((len = getln(line, MAXLINE)) > 0) {
+        reverse(line, reverse_line, len);
+
         if (len > max) {
             printf("longest-line size: %d, max permitted: %d\n", len, MAXLINE);
             max = len;
@@ -22,6 +34,8 @@ int main(void) {
         if (len >= LONGEST_CRITERION) {
             printf("\n%s\n", line);
         }
+
+        printf("reverse: %s\n", reverse_line);
     }
 
     if (max > 0) {
