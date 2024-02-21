a871825c99f1b4050e805bdb9e937cba2d618a06
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-21 12:45:29 -0300

    add extern variables

--- the_c_programming_language/01/char_array.c
+++ the_c_programming_language/01/char_array.c
@@ -4,32 +4,37 @@
 #define LONGEST_CRITERION 80
 #define REMOVE_NULL_CHAR 1
 
-int getln(char line[], int maxline);
-void copy(char to[], char from[]);
+int max;
+char line[MAXLINE];
+char longest[MAXLINE];
+char reverse_line[MAXLINE];
 
-void reverse(char s[], char to[], int size) {
+int getln();
+void copy();
+
+void reverse(int size) {
     int i;
+    extern char line[];
+    extern char reverse_line[];
 
     for (i = 0; i < size; i++) {
-        to[i] = s[(size - i) - REMOVE_NULL_CHAR];
+        reverse_line[i] = line[(size - i) - REMOVE_NULL_CHAR];
     }
 }
 
 int main(void) {
     int len;
-    int max;
-    char line[MAXLINE];
-    char longest[MAXLINE];
-    char reverse_line[MAXLINE];
+    extern int max;
+    extern char longest[MAXLINE];
 
     max = 0;
-    while ((len = getln(line, MAXLINE)) > 0) {
-        reverse(line, reverse_line, len);
+    while ((len = getln()) > 0) {
+        reverse(len);
 
         if (len > max) {
             printf("longest-line size: %d, max permitted: %d\n", len, MAXLINE);
             max = len;
-            copy(longest, line);
+            copy();
         }
         if (len >= LONGEST_CRITERION) {
             printf("\n%s\n", line);
@@ -45,34 +50,37 @@ int main(void) {
     return 0;
 }
 
-int getln(char s[], int lim) {
+int getln() {
     int c, i;
+    extern char line[];
 
-    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    for (i = 0; i < MAXLINE - 1 && (c = getchar()) != EOF && c != '\n'; i++) {
         while (c == ' ' || c == '\t') {
             c = getchar();
         }
 
-        s[i] = c;
+        line[i] = c;
     }
 
     if (c == '\n') {
-        s[i] = c;
+        line[i] = c;
         i++;
     }
 
-    s[i] = '\0';
+    line[i] = '\0';
 
     return i;
 }
 
-void copy(char to[], char from[]) {
+void copy() {
     int i;
+    extern char line[];
+    extern char longest[];
     
     i = 0;
-    while ((to[i] = from[i]) != '\0') {
+    while ((longest[i] = line[i]) != '\0') {
         i++;
     }
     
-    to[i] = '\0';
+    longest[i] = '\0';
 }
