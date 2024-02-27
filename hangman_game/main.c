991632d1f4518741801b852ef33f88965697ab74
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-27 14:02:33 -0300

    add in-game losing logic

--- hangman_game/main.c
+++ hangman_game/main.c
@@ -2,18 +2,81 @@
 #include <stdlib.h>
 
 #define BUFSIZE 1028
+#define MAX_ATTEMPS 3
+#define NEWLINE_AND_NULL_CHAR 2
+
+char right_words[BUFSIZE];
+int failed_attemps;
 
 char * get_word();
 
-int main(void) {
-  char * word = get_word();
+void check_guessed(char *word) {
+  int i;
+
+  i = 0;
+
+  while (i < (sizeof(word) - NEWLINE_AND_NULL_CHAR)) {
+    if (right_words[i] != '\0') {
+      printf(" %c ", right_words[i]);
+    } else {
+      printf(" _ ");
+    }
+
+    if ((i + 1) >= (sizeof(word) - NEWLINE_AND_NULL_CHAR)) {
+      printf("\n");
+    }
+    i++;
+  }
+}
+
+void check_char(int *c, char *word) {
+  int i;
+  int have_char;
+
+  i = 0;
+  have_char = 0;
+
+  do {
+    if (*c == word[i]) {
+      right_words[i] = *c;
+      have_char = 1;
+    }
+    i++;
+  } while (word[i] != '\0');
+
+  if (!have_char) failed_attemps++;
+}
+
+void game_loop(void) {
+  int c, *char_pointer;
+  char *word = get_word();
+  
+  failed_attemps = 0;
+  char_pointer = &c;
 
   printf("%s\n", word);
+
+  while ((c = getchar())) {
+    getchar(); // skips '\n'
+    check_char(char_pointer, word);
+    check_guessed(word);
+
+    printf("failed_attemps = %d\n", failed_attemps);
+
+    if (failed_attemps == MAX_ATTEMPS) {
+      printf("you lose.\n");
+      break;
+    }
+  }
+}
+
+int main(void) {
+  game_loop();
 
   return 0;
 }
 
-char * get_word() {
+char * get_word(void) {
   int i, c;
   int bufsize = BUFSIZE;
   char *word;
