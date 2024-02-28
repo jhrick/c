39188c2adf93e572368c94befde5bd09e029db84
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-28 12:54:54 -0300

    refactor: qtd_subjects dinamic

--- study_cicle_creator/src/main.c
+++ study_cicle_creator/src/main.c
@@ -1,134 +1,130 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define QTD_SUBJECTS 10
 #define MIN_STUDY_HOURS 2
 
-char * subjects[] = {
-    "math",
-    "portuguese",
-    "geography",
-    "history",
-    "biology",
-    "philosophy",
-    "sociology",
-    "chemical",
-    "physics",
-    "english"
+char *subjects[] = {
+"math",      "portuguese", "geography",
+"history",   "biology",    "philosophy",
+"sociology", "chemical",   "physics"
 };
 
+int qtd_subjects() { return sizeof(subjects) / sizeof(int *); }
+
 double round_hours(double hours) {
-    double rounded_hours;
-    int int_part;
-    double dec_part;
+  double rounded_hours;
+  int int_part;
+  double dec_part;
 
-    if (hours < MIN_STUDY_HOURS) {
-        rounded_hours = MIN_STUDY_HOURS;
+  if (hours < MIN_STUDY_HOURS) {
+    rounded_hours = MIN_STUDY_HOURS;
+  } else {
+    int_part = (int)hours;
+    dec_part = hours - int_part;
+
+    if (dec_part <= 5) {
+      rounded_hours = int_part;
     } else {
-        int_part = (int)hours;
-        dec_part = hours - int_part;
-
-        if (dec_part <= 5) {
-            rounded_hours = int_part;
-        } else {
-            rounded_hours = (int_part + 1);
-        }
+      rounded_hours = (int_part + 1);
     }
+  }
 
-    return rounded_hours;
+  return rounded_hours;
 }
 
-int * get_subject_difficulty(void) {
-    int i, c;
-    int *difficult_in_each_suject = malloc(QTD_SUBJECTS * sizeof(int));
+int *get_subject_difficulty(void) {
+  int i, c;
+  int *difficult_in_each_suject = malloc(qtd_subjects() * sizeof(int));
 
-    if (!difficult_in_each_suject) {
-        return NULL;
+  if (!difficult_in_each_suject) {
+    return NULL;
+  }
+
+  printf("1: excellent; 2: good; 3: ok; 4: bad; 5: too bad\n");
+
+  i = 0;
+
+  while (c != EOF) {
+    if (c != '\n') {
+      printf("%s: ", subjects[i]);
     }
 
-    printf("1: excellent; 2: good; 3: ok; 4: bad; 5: too bad\n");
+    c = getchar();
 
-    i = 0;
-
-    while (c != EOF) {
-        if (c != '\n') {
-            printf("%s: ", subjects[i]);
-        }
-
-        c = getchar();
-
-        if (c >= '1' && c <= '5') {
-            difficult_in_each_suject[i] = c-'0';
-            i++;
-        }
-
-        if (i == (QTD_SUBJECTS)) {
-            c = EOF;
-        }
+    if (c >= '1' && c <= '5') {
+      difficult_in_each_suject[i] = c - '0';
+      i++;
     }
-    return difficult_in_each_suject;
+
+    if (i == qtd_subjects()) {
+      c = EOF;
+    }
+  }
+  return difficult_in_each_suject;
 }
 
-double * calculate_study_hours_per_subject(int * difficult_in_each_suject) {
-    int i;
-    double hours_per_day, days_per_week;
-    int total_difficulty_hours;
-    double study_hours_per_difficulty;
-    double * subject_study_hours = malloc(QTD_SUBJECTS * sizeof(int));
+double *calculate_study_hours_per_subject(int *difficult_in_each_suject) {
+  int i;
+  double hours_per_day, days_per_week;
+  int total_difficulty_hours;
+  double study_hours_per_difficulty;
+  double *subject_study_hours = malloc(qtd_subjects() * sizeof(int));
 
-    if (!subject_study_hours) {
-        return NULL;
-    }
+  if (!subject_study_hours) {
+    return NULL;
+  }
 
-    total_difficulty_hours = 0;
+  total_difficulty_hours = 0;
 
-    for (i = 0; i < QTD_SUBJECTS; i++) {
-        total_difficulty_hours += difficult_in_each_suject[i];
-    }
-        printf("%d\n", total_difficulty_hours);
+  for (i = 0; i < qtd_subjects(); i++) {
+    total_difficulty_hours += difficult_in_each_suject[i];
+  }
 
-    printf("how many hours you want study per day? ");
+  printf("how many hours you want study per day? ");
 
-    getchar(); // consume newline
-    hours_per_day = getchar() - '0';
+  getchar(); // consume newline
+  hours_per_day = getchar() - '0';
 
-    printf("how many days you want study per week? ");
+  printf("how many days you want study per week? ");
 
-    getchar(); // consume newline
-    days_per_week = getchar() - '0';
+  getchar(); // consume newline
+  days_per_week = getchar() - '0';
 
-    study_hours_per_difficulty = (hours_per_day * days_per_week) / total_difficulty_hours;
+  study_hours_per_difficulty =
+      (hours_per_day * days_per_week) / total_difficulty_hours;
 
-    printf("%.2f, %d\n", (hours_per_day * days_per_week), total_difficulty_hours);
+  printf("%.2f, %d\n", (hours_per_day * days_per_week), total_difficulty_hours);
 
-    for (i = 0; i < QTD_SUBJECTS; i++) {
-        subject_study_hours[i] = round_hours(study_hours_per_difficulty * difficult_in_each_suject[i]);
-    }
+  for (i = 0; i < qtd_subjects(); i++) {
+    subject_study_hours[i] =
+        round_hours(study_hours_per_difficulty * difficult_in_each_suject[i]);
+  }
 
-    return subject_study_hours;
+  return subject_study_hours;
 }
 
 void create_study_cicle() {
-    int i;
-    int * difficult_in_each_suject;
-    double *subject_study_hours;
+  int i;
+  int *difficult_in_each_suject;
+  double *subject_study_hours;
 
-    difficult_in_each_suject = get_subject_difficulty();
+  difficult_in_each_suject = get_subject_difficulty();
 
-    subject_study_hours = calculate_study_hours_per_subject(difficult_in_each_suject);
+  subject_study_hours =
+      calculate_study_hours_per_subject(difficult_in_each_suject);
 
-    for (i = 0; i < QTD_SUBJECTS; i++) {
-        printf("%s: %2.2fhrs\n", subjects[i], subject_study_hours[i]);
-    }
+  for (i = 0; i < qtd_subjects(); i++) {
+    printf("%s: %2.2fhrs\n", subjects[i], subject_study_hours[i]);
+  }
 
-    free(difficult_in_each_suject);
-    free(subject_study_hours);
+  free(difficult_in_each_suject);
+  free(subject_study_hours);
 
-    return;
+  return;
 }
 
 int main(void) {
-    create_study_cicle();
+  create_study_cicle();
 
-    return 0;
+  return 0;
 }
