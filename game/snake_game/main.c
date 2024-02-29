348901969deb721a7158912e0c4e49cde0635630
Jorge Henrique <hj.rickcp@gmail.com>
2024-02-29 14:21:05 -0300

    snake_game: add grow logic

--- game/snake_game/main.c
+++ game/snake_game/main.c
@@ -1,69 +1,103 @@
-// #include <stdio.h>
+#include <ncurses.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <ncurses.h>
 
 #define WIDTH 10
 #define HEIGHT 30
+
 #define X_POSITION 0
 #define Y_POSITION 1
+#define DEFAULT_POSITION_VALUE 0
+#define SPEED 1
+
 #define MAX_SNAKE_SIZE 10 * 30
 #define POSTIONS 2
-#define SNAKE_HEAD 1
-#define SNAKE_FIRST_BODY_PART 1
+#define SNAKE_HEAD 0
 
-enum directions {
-    UP,
-    DOWN,
-    LEFT,
-    RIGHT
-};
+enum directions { UP, DOWN, LEFT, RIGHT };
 
 struct snake_schema {
-    int lenght;
-    int head[POSTIONS];
-    int body[MAX_SNAKE_SIZE][POSTIONS];
+  int lenght;
+  int body[MAX_SNAKE_SIZE][POSTIONS];
 };
 
 struct apple_schema {
-    int x;
-    int y;
+  int x;
+  int y;
 };
 
-void snake_moviment(enum directions direction);
-
 enum directions last_direction;
+struct snake_schema snake;
 int snake_lenght;
 int snake_head_positions[POSTIONS]; // x and y position
 
-struct snake_schema snake_initialize() {
-  int i;  
+void snake_moviment(enum directions direction) {
+  int i;
+
+  for (i = (snake_lenght - 1); i > 0; i--) {
+    snake.body[i][X_POSITION] = (snake.body[i - 1][X_POSITION]);
+    snake.body[i][Y_POSITION] = (snake.body[i - 1][Y_POSITION]);
+  }
+
+  switch (direction) {
+  case UP:
+    if (last_direction != DOWN) {
+      snake_head_positions[X_POSITION]--;
+      snake.body[SNAKE_HEAD][X_POSITION] = snake_head_positions[X_POSITION];
+    }
+    last_direction = UP;
+    break;
+  case DOWN:
+    if (last_direction != UP) {
+      snake_head_positions[X_POSITION]++;
+      snake.body[SNAKE_HEAD][X_POSITION] = snake_head_positions[X_POSITION];
+    }
+    last_direction = DOWN;
+    break;
+  case LEFT:
+    if (last_direction != RIGHT) {
+      snake_head_positions[Y_POSITION]--;
+      snake.body[SNAKE_HEAD][Y_POSITION] = snake_head_positions[Y_POSITION];
+    }
+    last_direction = LEFT;
+    break;
+  case RIGHT:
+    if (last_direction != LEFT) {
+      snake_head_positions[Y_POSITION]++;
+      snake.body[SNAKE_HEAD][Y_POSITION] = snake_head_positions[Y_POSITION];
+    }
+    last_direction = RIGHT;
+    break;
+  }
+}
+
+struct snake_schema snake_initialize(void) {
+  int i;
   struct snake_schema snake;
 
-  snake.head[X_POSITION] = snake_head_positions[X_POSITION];
-  snake.head[Y_POSITION] = snake_head_positions[Y_POSITION];
+  snake.body[SNAKE_HEAD][X_POSITION] = snake_head_positions[X_POSITION];
+  snake.body[SNAKE_HEAD][Y_POSITION] = snake_head_positions[Y_POSITION];
 
-  for (i = 0; i < snake_lenght; i++) {
-    snake.body[i][X_POSITION] = (snake.head[X_POSITION] - i);
-    snake.body[i][Y_POSITION] = (snake.head[Y_POSITION] - i);
-  }
+  snake.body[1][X_POSITION] = (snake.body[SNAKE_HEAD][X_POSITION] - 1);
+  snake.body[1][Y_POSITION] = (snake.body[SNAKE_HEAD][Y_POSITION] - 1);
 
   return snake;
 }
 
 struct apple_schema generate_apple() {
-    struct apple_schema apple;
-    int r = rand();
-    
-    apple.x = r % WIDTH;
-    apple.y = r % HEIGHT;
+  struct apple_schema apple;
+  int r = rand();
 
-    return apple;
+  apple.x = r % WIDTH;
+  apple.y = r % HEIGHT;
+
+  return apple;
 }
 
 bool snake_eat_apple(struct snake_schema snake, struct apple_schema apple) {
-  if (snake.head[X_POSITION] == apple.x && snake.head[Y_POSITION] == apple.y) {
-      return 1;
+  if (snake.body[SNAKE_HEAD][X_POSITION] == apple.x &&
+      snake.body[SNAKE_HEAD][Y_POSITION] == apple.y) {
+    return 1;
   }
 
   return 0;
@@ -72,56 +106,65 @@ bool snake_eat_apple(struct snake_schema snake, struct apple_schema apple) {
 enum directions key_handler(int arrow) {
   enum directions direction;
 
-  switch (arrow)  {
-    case KEY_UP: 
-      direction = UP;
-      printw("\nup\n");
-      break;
-    case KEY_DOWN: 
-      direction = DOWN;
-      printw("\ndown\n");
-      break;
-    case KEY_LEFT: 
-      direction = LEFT;
-      printw("\nleft\n");
-      break;
-    case KEY_RIGHT: 
-      direction = RIGHT;
-      printw("\nright\n");
-      break;
-  } 
+  switch (arrow) {
+  case KEY_UP:
+    direction = UP;
+    break;
+  case KEY_DOWN:
+    direction = DOWN;
+    break;
+  case KEY_LEFT:
+    direction = LEFT;
+    break;
+  case KEY_RIGHT:
+    direction = RIGHT;
+    break;
+  }
 
   return direction;
 }
 
-void game_panel(struct snake_schema snake, struct apple_schema apple) {
+int render_snake(int width, int height, struct snake_schema snake) {
   int i;
+
+  for (i = 0; i < snake_lenght; i++) {
+    if (snake.body[i][X_POSITION] == width &&
+        snake.body[i][Y_POSITION] == height) {
+      printw(" # ");
+      return 1;
+    }
+  }
+
+  return 0;
+}
+
+void game_panel(struct snake_schema snake, struct apple_schema apple) {
   int width, height;
 
-  printw("x: %d, y: %d\n\n", snake.head[X_POSITION], snake.head[Y_POSITION]);
-  printw("apple: %d %d\n", apple.x, apple.y);
+  clear();
 
-  for (width = 0; width <= WIDTH; width++) {
-    i = 0;
+  for (width = 0; width < WIDTH; width++) {
     for (height = 0; height < HEIGHT; height++) {
-      if (snake.head[X_POSITION] == width && snake.head[Y_POSITION] == height) {
-        printw("# ");
+      if (render_snake(width, height, snake)) {
+        continue;
       } else if (apple.x == width && apple.y == height) {
-        printw("* ");
+        printw(" * ");
       } else {
-        printw(". ");
+        printw(" . ");
       }
     }
     printw("\n");
   }
+  refresh();
 }
 
 int main(void) {
   int ch;
 
-  struct snake_schema snake;
   enum directions direction;
   struct apple_schema apple;
+  
+  snake_head_positions[X_POSITION] = snake_head_positions[Y_POSITION] = DEFAULT_POSITION_VALUE;
 
   snake_lenght = 3;
 
@@ -129,26 +172,24 @@ int main(void) {
   direction = RIGHT;
 
   apple = generate_apple();
+  snake = snake_initialize();
 
   initscr();
   keypad(stdscr, true);
   nodelay(stdscr, TRUE);
 
   while (1) {
-    printw("lenght: %d | ", snake_lenght);
     ch = getch();
 
-    timeout(50);
+    timeout(150);
 
-    if ((ch = getch()) == ERR) {
-      snake_moviment(direction);
-    } else {
+    if ((ch = getch()) != ERR) {
       direction = key_handler(ch);
     }
 
     mvprintw(0, 0, "\033[H\033[J"); /* clear screen */
-
-    snake = snake_initialize();
+    
+    snake_moviment(direction);
 
     if (snake_eat_apple(snake, apple)) {
       apple = generate_apple();
@@ -159,25 +200,4 @@ int main(void) {
   }
   endwin();
   return 0;
-}
-
-void snake_moviment(enum directions direction) {
-  switch (direction) {
-    case UP:
-      if (last_direction != DOWN) snake_head_positions[X_POSITION]--;
-      last_direction = UP;
-      break;
-    case DOWN:
-      if (last_direction != UP) snake_head_positions[X_POSITION]++;
-      last_direction = DOWN;
-      break;
-    case LEFT:
-      if (last_direction != RIGHT) snake_head_positions[Y_POSITION]--;
-      last_direction = LEFT;
-      break;
-    case RIGHT:
-      if (last_direction != LEFT) snake_head_positions[Y_POSITION]++;
-      last_direction = RIGHT;
-      break;
-  }
 }
